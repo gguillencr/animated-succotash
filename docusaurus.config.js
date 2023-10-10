35ae6b0a492dc35f3a051b7a689402fad02023f2
Gonzalo Guillén Rivera <gonzalo.guillen@softcial.com>
2023-10-10 14:54:08 -0600

    middlit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Softcial\'s Wiki',
+  tagline: 'Dinosaurs are cool and nice!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Softcial\'s Wiki',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -76,7 +76,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Internal Projects',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
